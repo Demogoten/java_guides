583776bbba60bf487f105168281da5d37576fd38
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-09-06 16:32:07 +0300

    https://www.youtube.com/watch?v=jWTjHmuuvJs 1:05:56

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -4,14 +4,16 @@
 import java.util.Scanner;
 public class apples {
     public static void main(String args[]) {
-      Scanner bucky = new Scanner(System.in);
-        double fnum,snum,answer;
-        System.out.println("Enter first num: ");
-        fnum = bucky.nextDouble();
-        System.out.println("Enter second num: ");
-        snum = bucky.nextDouble();
-        answer = fnum+snum;
-        System.out.println(answer);
+      int test = 6;
+
+        if (test == 9)
+                        {
+            System.out.println("yes");
+                        }
+            else
+                {
+            System.out.println("this is else");
+                }
 
     }
 }
