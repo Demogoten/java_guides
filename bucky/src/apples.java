0ed7492621a0f78a49405cd9c83c7a501822585e
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-09-06 18:23:31 +0300

    https://www.youtube.com/watch?v=jWTjHmuuvJs 1:22:22

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -4,25 +4,11 @@
 import java.util.Scanner;
 public class apples {
     public static void main(String args[]) {
-        int age;
-        age = 3;
-        switch (age) {
-            case 1:
-                System.out.println("You can crawl");
-                break;
-            case 2:
-                System.out.println("You can talk");
-                break;
-            case 3:
-                System.out.println("You can get in trouble");
-                break;
-            default:
-                System.out.println("I dont know how old you are");
-                break;
-
-
-
+        int counter =0;
+        while(counter<10){
+            System.out.println(counter);
+            counter++;
         }
     }
-
+}
 
