ea41f0389733bad8b9b0580d1db52e460b5ad5b5
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-09-06 20:19:35 +0300

    1:28:55

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -4,11 +4,9 @@
 import java.util.Scanner;
 public class apples {
     public static void main(String args[]) {
-        int counter =0;
-        while(counter<10){
-            System.out.println(counter);
-            counter++;
-        }
+
+        tuna tunaObject = new tuna();
+        tunaObject.simpleMessage();
     }
 }
 
---
+++ bucky/src/tuna.java
@@ -0,0 +1,8 @@
+/**
+ * Created by FlorinNitoiu on 9/6/2015.
+ */
+public class tuna {
+    public void simpleMessage(){
+        System.out.println("this is another class");
+    }
+}
