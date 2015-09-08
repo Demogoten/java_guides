2c2f16dedc37cc3b405be4afd8a7a3ce8d1fc8f9
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-09-08 07:25:44 +0300

    2:57:10 arrays exercices, ++freq[1+rand.nextInt(6)];  AWESOME

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -5,11 +5,16 @@ import java.util.Random;
 public class apples {
     public static void main(String args[]) {
 
-        System.out.println("index\t value");
-        int bucky[]={32,12,16,54,2};
+        Random rand = new Random();
+        int freq[]=new int[7];
 
-        for(int counter=0;counter<bucky.length;counter++){
-            System.out.println(counter + "\t" + bucky[counter]);
+        for (int roll=1;roll<1000;roll++){
+            ++freq[1+rand.nextInt(6)];
+        }
+        System.out.println("Face\tFrequency");
+
+        for (int face=1;face<freq.length;face++){
+            System.out.println(face+"\t"+freq[face]);
         }
     }
 }
