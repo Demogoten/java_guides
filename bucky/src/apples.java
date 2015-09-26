0c18c96f2915c9093b25a15aeaacf979c456f6bc
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-09-26 07:12:35 +0300

    JOptionPane.showInputDialog

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -1,21 +1,16 @@
-
-//import java.util.Scanner;
-import java.util.Random;
-
+import javax.swing.JOptionPane;
 public class apples {
     public static void main(String args[]) {
 
-        Random rand = new Random();
-        int freq[]=new int[7];
+        String fn = JOptionPane.showInputDialog("Enter first number: ");
+        String sn = JOptionPane.showInputDialog("Enter second number: ");
 
-        for (int roll=1;roll<1000;roll++){
-            ++freq[1+rand.nextInt(6)];
-        }
-        System.out.println("Face\tFrequency");
+        int num1 = Integer.parseInt(fn);
+        int num2 = Integer.parseInt(sn);
+        int sum = num1 + num2;
 
-        for (int face=1;face<freq.length;face++){
-            System.out.println(face+"\t"+freq[face]);
-        }
-    }
+        JOptionPane.showMessageDialog(null, "The answer is " + sum , "the title" , JOptionPane.PLAIN_MESSAGE);
+
+      }
 }
 
