51f21973f5c4ede0ab677ee90371f21204d0c099
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-10-07 20:20:20 +0300

    JOptionPane.showInputDialog

--- bucky/src/apples.java
+++ bucky/src/apples.java
@@ -1,16 +1,13 @@
-import javax.swing.JOptionPane;
+import javax.swing.JFrame;
+
+
 public class apples {
     public static void main(String args[]) {
 
-        String fn = JOptionPane.showInputDialog("Enter first number: ");
-        String sn = JOptionPane.showInputDialog("Enter second number: ");
-
-        int num1 = Integer.parseInt(fn);
-        int num2 = Integer.parseInt(sn);
-        int sum = num1 + num2;
-
-        JOptionPane.showMessageDialog(null, "The answer is " + sum , "the title" , JOptionPane.PLAIN_MESSAGE);
-
+        tuna bucky = new tuna();
+        bucky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        bucky.setSize(350, 100);
+        bucky.setVisible(true);
       }
 }
 
