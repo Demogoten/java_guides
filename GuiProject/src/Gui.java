a06ed28eb1a3c926b448372ba8c55070accc8187
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-10-28 19:24:44 +0200

    MouseListener override 5 methods , MouseMotionListener override 2 methods , Mouse events !

--- GuiProject/src/Gui.java
+++ GuiProject/src/Gui.java
@@ -1,44 +1,57 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.peer.MouseInfoPeer;
 import javax.swing.*;
 import javax.swing.event.*;
 
 public class Gui extends JFrame{
-
-    private JList leftlist;
-    private JList rightlist;
-    private JButton movebutton;
-    private static String[] foods ={"bacon", "wings","ham", "beef", "morebacon"};
+    private JPanel mousepanel;
+    private JLabel statusbar;
 
     public Gui(){
         super("title");
-        setLayout(new FlowLayout());
 
-        leftlist = new JList(foods);
-        leftlist.setVisibleRowCount(4);
-        leftlist.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        add(new JScrollPane(leftlist));
+        mousepanel = new JPanel();
+        mousepanel.setBackground(Color.WHITE);
+        add(mousepanel, BorderLayout.CENTER);
 
-        movebutton = new JButton("Move -->");
-        movebutton.addActionListener(
-                new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent event) {
-                    rightlist.setListData(leftlist.getSelectedValues());
-                    }
-                }
-        );
+        statusbar = new JLabel("default");
+        add(statusbar, BorderLayout.SOUTH);
 
-        add(movebutton);
-
-        rightlist = new JList();
-        rightlist.setVisibleRowCount(4);
-        rightlist.setFixedCellWidth(110);
-        rightlist.setFixedCellHeight(15);
-        rightlist.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-        add(new JScrollPane(rightlist));
-
-
+        HandlerClass handler = new HandlerClass();
+        mousepanel.addMouseListener(handler);
+        mousepanel.addMouseMotionListener(handler);
     }
 
+    private class HandlerClass implements MouseListener,MouseMotionListener{
+
+        public void mouseClicked(MouseEvent event){
+        statusbar.setText(String.format("Clicked at %d,%d", event.getX(),event.getY()));
+        }
+        public void mousePressed(MouseEvent event){
+            statusbar.setText("you pressed down the mouse");
+        }
+        public void mouseReleased(MouseEvent event){
+            statusbar.setText("you released the button");
+        }
+        public void mouseEntered(MouseEvent event){
+            statusbar.setText("you entered the area");
+            mousepanel.setBackground(Color.RED);
+        }
+        public void mouseExited(MouseEvent event){
+            statusbar.setText("The mouse has left the window");
+            mousepanel.setBackground(Color.WHITE);
+        }
+        //these are mouse motion events
+
+
+        @Override
+        public void mouseDragged(MouseEvent event) {
+            statusbar.setText("you are dragging the mouse");
+        }
+        public void mouseMoved(MouseEvent event) {
+            statusbar.setText("you moved the mouse");
+        }
+
+    }
 }
