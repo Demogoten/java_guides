ee72857aba6bf6dff6c03204f8cfe9e44b6e58d4
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-10-29 08:28:18 +0200

    event.isMetaDown()   !?? event.isAltDown()  !??
    simulates if you have a 1-2-or 3 button mouses

--- GuiProject/src/Gui.java
+++ GuiProject/src/Gui.java
@@ -5,53 +5,29 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 public class Gui extends JFrame{
-    private JPanel mousepanel;
+    private String details;
     private JLabel statusbar;
 
     public Gui(){
-        super("title");
+        super("Title");
 
-        mousepanel = new JPanel();
-        mousepanel.setBackground(Color.WHITE);
-        add(mousepanel, BorderLayout.CENTER);
-
-        statusbar = new JLabel("default");
+        statusbar = new JLabel("This is default");
         add(statusbar, BorderLayout.SOUTH);
+        addMouseListener(new Mouseclass());
 
-        HandlerClass handler = new HandlerClass();
-        mousepanel.addMouseListener(handler);
-        mousepanel.addMouseMotionListener(handler);
     }
 
-    private class HandlerClass implements MouseListener,MouseMotionListener{
-
+    private class Mouseclass extends MouseAdapter{
         public void mouseClicked(MouseEvent event){
-        statusbar.setText(String.format("Clicked at %d,%d", event.getX(),event.getY()));
-        }
-        public void mousePressed(MouseEvent event){
-            statusbar.setText("you pressed down the mouse");
-        }
-        public void mouseReleased(MouseEvent event){
-            statusbar.setText("you released the button");
-        }
-        public void mouseEntered(MouseEvent event){
-            statusbar.setText("you entered the area");
-            mousepanel.setBackground(Color.RED);
-        }
-        public void mouseExited(MouseEvent event){
-            statusbar.setText("The mouse has left the window");
-            mousepanel.setBackground(Color.WHITE);
-        }
-        //these are mouse motion events
+            details = String.format("You clicked %d ", event.getClickCount());
 
+            if (event.isMetaDown())
+                details += "with right mouse button";
+            else if(event.isAltDown())
+                details += "either center mouse button";
+            else details += "with left mouse button";
 
-        @Override
-        public void mouseDragged(MouseEvent event) {
-            statusbar.setText("you are dragging the mouse");
+            statusbar.setText(details);
         }
-        public void mouseMoved(MouseEvent event) {
-            statusbar.setText("you moved the mouse");
-        }
-
     }
 }
