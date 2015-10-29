7c61891232f9442410746c71f8dac9818e099425
Alexandru Nitoiu <alex_nitoiu@yahoo.com>
2015-10-29 11:10:35 +0200

    Formatter

--- PlayingWithFiles/src/Apples.java
+++ PlayingWithFiles/src/Apples.java
@@ -1,14 +1,20 @@
-import java.io.File;
+import java.util.*;
 
 
 public class Apples {
     public static void main(String[] args){
-        File x = new File("C:\\test\\greg.txt");
 
-        if(x.exists())
-            System.out.println(x.getName() + " exist!");
-        else
-            System.out.println("This thing doesn't exists");
+        final Formatter x;  //outputs Strings into a file , formatter variables , printing to the file
+
+        try{
+            x = new Formatter("fred.txt"); //if you dont have this file he creates it for you
+            System.out.println("you created a file");
+        }
+        catch(Exception e){
+            System.out.println("you got an error");
+        }
+
+
     }
 
 }
